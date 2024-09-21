d64d84081856558a027b8c9cb9df5489f4b0304d
GilangPradnyana <gilangpradnyana2@gmail.com>
2024-09-21 13:58:48 +0800

    added the funcamental of the statement to addItem, edit and if the client didn`t input any value

--- app.js
+++ app.js
@@ -16,7 +16,20 @@ form.addEventListener('submit', addItems);
 // window.addEventListener('DOMContentLoaded', setupItems);
 
 // ****** FUNCTIONS **********
-
+function addItems(e) {
+    e.preventDefault();
+    // get the input value and the special ID
+    const value = grocery.value;
+    const ID = new Date().getTime().toString();
+    // make the deccison for addItem, edit and nothing
+    if(value && !editFlag) {
+        console.log('hi');
+    } else if(value && editFlag) {
+        console.log('hello');
+    } else { 
+        console.log('please add some items')
+    };
+};
 // ****** LOCAL STORAGE **********
 
 // ****** SETUP ITEMS **********
